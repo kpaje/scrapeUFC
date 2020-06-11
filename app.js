3ebdb7b1c5f6e83423a3f307d09e4f89b93114d4
kpaje <kelvinmembership@gmail.com>
2020-06-11 11:53:56 -0700

    moved functions to own scrape module

--- app.js
+++ app.js
@@ -1,74 +1,46 @@
 const puppeteer = require("puppeteer");
-
-function ifDocument(request) {
-	if (request.resourceType() === "document") {
-		request.continue();
-	} else {
-		request.abort();
-	}
-}
-
-async function awaitMoreSelectors(currentPage, pagesToScrape) {
-	if (currentPage < pagesToScrape) {
-		await Promise.all([
-			await page.waitForSelector("a.morelink"),
-			await page.click("a.morelink"),
-			await page.waitForSelector("a.storylink"),
-		]);
-	}
-}
-
-function verifyPages(pagesToScrape) {
-	if (!pagesToScrape) {
-		pagesToScrape = 1;
-	}
-}
-
-async function requestPage(page) {
-	await page.setRequestInterception(true);
-	page.on("request", (request) => {
-		ifDocument(request);
-	});
-}
-
-async function awaitPageURL(page) {
-	await page.goto("https://news.ycombinator.com/");
-}
+const {
+  awaitMoreSelectors,
+  verifyPages,
+  requestPage,
+  awaitPageURL,
+} = require("./scrape.js");
 
 function app(pagesToScrape) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const browser = await puppeteer.launch();
-			const page = await browser.newPage();
-			let currentPage = 1;
-			let urls = [];
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      let currentPage = 1;
+      let urls = [];
+      let site = "https://news.ycombinator.com/";
 
-			verifyPages(pagesToScrape);
-			requestPage(page);
-			awaitPageURL(page);
+      verifyPages(pagesToScrape);
+      requestPage(page);
+      awaitPageURL(page, site);
 
-			while (currentPage <= pagesToScrape) {
-				await page.waitForSelector("a.storylink");
-				let newUrls = await page.evaluate(() => {
-					let results = [];
-					let items = document.querySelectorAll("a.storylink");
-					items.forEach((item) => {
-						results.push({
-							url: item.getAttribute("href"),
-							text: item.innerText,
-						});
-					});
-					return results;
-				});
-				urls = urls.concat(newUrls);
-				awaitMoreSelectors(currentPage, pagesToScrape);
-				currentPage++;
-			}
-			browser.close();
-			return resolve(urls);
-		} catch (e) {
-			return reject(e);
-		}
-	});
+      while (currentPage <= pagesToScrape) {
+        await page.waitForSelector("a.storylink");
+        let newUrls = await page.evaluate(() => {
+          let results = [];
+          let items = document.querySelectorAll("a.storylink");
+          items.forEach((item) => {
+            results.push({
+              url: item.getAttribute("href"),
+              text: item.innerText,
+            });
+          });
+          return results;
+        });
+        urls = urls.concat(newUrls);
+        awaitMoreSelectors(currentPage, pagesToScrape);
+        currentPage++;
+      }
+      browser.close();
+      return resolve(urls);
+    } catch (e) {
+      return reject(e);
+    }
+  });
 }
 app(1).then(console.log).catch(console.error);
