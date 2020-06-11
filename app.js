7b17f47d6e95573dbf2c4a744b0783d9061b38cc
kpcruz <kelvinmembership@gmail.com>
2020-06-11 05:02:32 -0700

    refactored scraper

--- app.js
+++ app.js
@@ -1,55 +1,87 @@
-/**
- * Copyright 2017 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Search developers.google.com/web for articles tagged
- * "Headless Chrome" and scrape results from the results page.
- */
-
-"use strict";
-
 const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+function ifDocument(request) {
+	if (request.resourceType() === "document") {
+		request.continue();
+	} else {
+		request.abort();
+	}
+}
 
-  await page.goto("https://developers.google.com/web/");
+async function awaitMoreSelectors(currentPage, pagesToScrape) {
+	if (currentPage < pagesToScrape) {
+		await Promise.all([
+			await page.waitForSelector("a.morelink"),
+			await page.click("a.morelink"),
+			await page.waitForSelector("a.storylink"),
+		]);
+	}
+}
 
-  // Type into search box.
-  await page.type("#searchbox input", "Headless Chrome");
+function verifyPages(pagesToScrape) {
+	if (!pagesToScrape) {
+		pagesToScrape = 1;
+	}
+}
 
-  // Wait for suggest overlay to appear and click "show all results".
-  const allResultsSelector = ".devsite-suggest-all-results";
-  await page.waitForSelector(allResultsSelector);
-  await page.click(allResultsSelector);
+async function requestPage(page) {
+	await page.setRequestInterception(true);
+	page.on("request", (request) => {
+		ifDocument(request);
+	});
+}
 
-  // Wait for the results page to load and display the results.
-  const resultsSelector = ".gsc-results .gsc-thumbnail-inside a.gs-title";
-  await page.waitForSelector(resultsSelector);
+async function awaitPageURL(page) {
+	await page.goto("https://news.ycombinator.com/");
+}
 
-  // Extract the results from the page.
-  const links = await page.evaluate((resultsSelector) => {
-    const anchors = Array.from(document.querySelectorAll(resultsSelector));
-    return anchors.map((anchor) => {
-      const title = anchor.textContent.split("|")[0].trim();
-      return `${title} - ${anchor.href}`;
-    });
-  }, resultsSelector);
-  console.log(links.join("\n"));
+function res() {
+	let results = [];
+	let items = document.querySelectorAll("a.storylink");
 
-  await browser.close();
-})();
+	items.forEach((item) => {
+		results.push({
+			url: item.getAttribute("href"),
+			text: item.innerText,
+		});
+	});
+	return results;
+}
+
+function app(pagesToScrape) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			let currentPage = 1;
+			let urls = [];
+
+			verifyPages(pagesToScrape);
+			requestPage(page);
+			awaitPageURL(page);
+
+			while (currentPage <= pagesToScrape) {
+				await page.waitForSelector("a.storylink");
+				let newUrls = await page.evaluate(() => {
+					let results = [];
+					let items = document.querySelectorAll("a.storylink");
+					items.forEach((item) => {
+						results.push({
+							url: item.getAttribute("href"),
+							text: item.innerText,
+						});
+					});
+					return results;
+				});
+				urls = urls.concat(newUrls);
+				awaitMoreSelectors(currentPage, pagesToScrape);
+				currentPage++;
+			}
+			browser.close();
+			return resolve(urls);
+		} catch (e) {
+			return reject(e);
+		}
+	});
+}
+app(1).then(console.log).catch(console.error);
