c338fd5cc8964c17542acc7715a72fa8f8567fc5
kpaje <kelvinmembership@gmail.com>
2020-06-15 16:23:53 -0700

    consolidated code and removed redundancy

--- app.js
+++ app.js
@@ -9,31 +9,56 @@ const {
 function app(pagesToScrape) {
   return new Promise(async (resolve, reject) => {
     try {
+      // open the browser
       const browser = await puppeteer.launch();
+      // open a new page
       const page = await browser.newPage();
       let currentPage = 1;
       let urls = [];
-      let site = "https://news.ycombinator.com/";
+      let site = "https://www.ufc.com/athletes/all";
+      let selector = "span.c-listing-athlete__name";
 
       verifyPages(pagesToScrape);
       requestPage(page);
+      // enter url in page
       awaitPageURL(page, site);
+      await page.waitForSelector(selector);
 
       while (currentPage <= pagesToScrape) {
-        await page.waitForSelector("a.storylink");
         let newUrls = await page.evaluate(() => {
+          let athleteProfiles = document.querySelectorAll("a.e-button--black ");
+          let athleteNames = document.querySelectorAll(
+            "span.c-listing-athlete__name"
+          );
+          let weightclasses = document.querySelectorAll(
+            "span.c-listing-athlete__title"
+          );
+          let records = document.querySelectorAll(
+            "span.c-listing-athlete__record"
+          );
+          let nicknames = document.querySelectorAll(
+            "span.c-listing-athlete__nickname"
+          );
+          let socialProfiles = document.querySelectorAll(
+            "a.c-listing-athlete-flipcard__social-link"
+          );
           let results = [];
-          let items = document.querySelectorAll("a.storylink");
-          items.forEach((item) => {
-            results.push({
-              url: item.getAttribute("href"),
-              text: item.innerText,
-            });
-          });
+
+          for (var i = 0; i < athleteProfiles.length; i++) {
+            results[i] = {
+              name: athleteNames[i].innerText,
+              nickname: nicknames[i].innerText,
+              profile: athleteProfiles[i].getAttribute("href"),
+              record: records[i].innerText,
+              weightclass: weightclasses[i].innerText,
+              socialProfile: socialProfiles[i].getAttribute("href"),
+            };
+          }
+
           return results;
         });
         urls = urls.concat(newUrls);
-        clickLinkSelector(currentPage, pagesToScrape, "a.storylink");
+        clickLinkSelector(currentPage, pagesToScrape, selector);
         currentPage++;
       }
       browser.close();
