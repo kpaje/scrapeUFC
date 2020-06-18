f24ac4171e229c09a5b704a5853c00f7bcafa1be
kpaje <kelvinmembership@gmail.com>
2020-06-18 16:54:29 -0700

    pulling events page

--- events.js
+++ events.js
@@ -5,11 +5,11 @@ const {
   requestPage,
   awaitPageURL,
 } = require("./scrape.js");
-const { selectors } = require("./selectors");
+// const { selectors } = require("./selectors");
 let currentPage = 1;
 let atheleteObject = [];
-let site = "https://www.ufc.com/athlete/israel-adesanya";
-let mainSelector = "div.c-hero--full__container";
+let site = "https://www.ufc.com/events";
+let mainSelector = "div.details-wrapper";
 
 function app(pagesToScrape) {
   return new Promise(async (resolve, reject) => {
@@ -23,20 +23,25 @@ function app(pagesToScrape) {
       await page.waitForSelector(mainSelector);
 
       while (currentPage <= pagesToScrape) {
-        let athleteProfile = await page.evaluate(
-          ({ selectors }) => {
-            let results = {};
+        let athleteProfile = await page.evaluate(() => {
+          //   ({ selectors }) => {
+          let headliners = "h3.c-card-event--result__headline";
+          //   let date = "div.data-main-card";
 
-            for (const names in selectors) {
-              const item = selectors[names];
-              Object.assign(results, {
-                [names]: document.querySelectorAll(item)[0].innerText,
-              });
-            }
-            return results;
-          },
-          { selectors }
-        );
+          const selectors = {
+            headliners,
+            // date,
+          };
+          let results = {};
+
+          for (const names in selectors) {
+            const item = selectors[names];
+            Object.assign(results, {
+              [names]: document.querySelectorAll(item)[0].innerText,
+            });
+          }
+          return results;
+        });
         atheleteObject = atheleteObject.concat(athleteProfile);
         clickLinkSelector(currentPage, pagesToScrape, mainSelector);
         currentPage++;
