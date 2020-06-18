a646667d1f2569f50602bb409eb7dde11e14e85e
kpaje <kelvinmembership@gmail.com>
2020-06-18 01:43:16 -0700

    exporting function injections

--- athlete.js
+++ athlete.js
@@ -6,37 +6,11 @@ const {
   awaitPageURL,
 } = require("./scrape.js");
 const { selectors } = require("./selectors");
-// const { profile } = require("./objectAssigns");
+const { profile, record, striking } = require("./objectAssigns");
 let currentPage = 1;
 let atheleteObject = [];
 let site = "https://www.ufc.com/athlete/israel-adesanya";
 let mainSelector = "div.c-hero--full__container";
-
-function profile(
-  results,
-  athleteNickname,
-  athleteStatus,
-  athleteHometown,
-  athletePhysicalStats
-) {
-  Object.assign(results.profile, {
-    nickname: athleteNickname[0].innerText,
-    status: athleteStatus[0].innerText,
-    hometown: athleteHometown[0].innerText,
-    physical: athletePhysicalStats[0].innerText,
-  });
-}
-function record(results, athleteWins) {
-  Object.assign(results.record, {
-    wins: athleteWins[0].innerText,
-  });
-}
-function striking(results, athleteStrikingAccuracy, athleteStrikesLanded) {
-  Object.assign(results.striking, {
-    strikingAccuracy: athleteStrikingAccuracy[0].innerText,
-    athleteStrikesLanded: athleteStrikesLanded[0].innerText,
-  });
-}
 
 function app(pagesToScrape) {
   return new Promise(async (resolve, reject) => {
--- objectAssigns.js
+++ objectAssigns.js
@@ -1,27 +1,31 @@
-let nickname = "div.c-hero--full__headline-prefix";
-let status = "div.c-bio__row--1col";
-let hometown = "div.c-bio__row--2col";
-let physicalStats = "div.c-bio__row--3col";
-let wins = "div.c-record__promoted-figure";
-let strikingAccuracy = "div.e-chart-circle__wrapper";
-let strikesLanded = "dd.c-overlap__stats-value";
-
-async function profile() {
-//   results,
-//   athleteNickname,
-//   athleteStatus,
-//   athleteHometown,
-//   athletePhysicalStats
-  //   let profile = Object.assign(results.profile, {
-  //     nickname: athleteNickname[0].innerText,
-  //     status: athleteStatus[0].innerText,
-  //     hometown: athleteHometown[0].innerText,
-  //     physical: athletePhysicalStats[0].innerText,
-  //   });
-  console.log("profile");
-  //   console.log(profile);
+function profile(
+  results,
+  athleteNickname,
+  athleteStatus,
+  athleteHometown,
+  athletePhysicalStats
+) {
+  Object.assign(results.profile, {
+    nickname: athleteNickname[0].innerText,
+    status: athleteStatus[0].innerText,
+    hometown: athleteHometown[0].innerText,
+    physical: athletePhysicalStats[0].innerText,
+  });
+}
+function record(results, athleteWins) {
+  Object.assign(results.record, {
+    wins: athleteWins[0].innerText,
+  });
+}
+function striking(results, athleteStrikingAccuracy, athleteStrikesLanded) {
+  Object.assign(results.striking, {
+    strikingAccuracy: athleteStrikingAccuracy[0].innerText,
+    athleteStrikesLanded: athleteStrikesLanded[0].innerText,
+  });
 }
 
 module.exports = {
   profile,
+  record,
+  striking,
 };
